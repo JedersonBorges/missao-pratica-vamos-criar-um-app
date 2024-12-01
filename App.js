e850da6ce775adb01b93f7f7b9f45da14aff748d
Jederson Borges <jedersonborges16@gmail.com>
2024-11-30 21:28:29 -0300

    Initial commit - React Native app

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { VirtualizedList, Text, View } from 'react-native';
+import PizzaTranslator from './pizzaTranslator';
+import ScrollViewExample from './ScrollView';
+import FlatListBasics from './FlatListBasics';
+import SectionListBasics from './SectionListBasics';
+import DisplayAnImage from './DisplayAnImage'; 
+import Cat from './Cat';  
+import Cafe from './Cafe';  
+import CatApp from './CatApp';  
 
-export default function App() {
+const App = () => {
+
+  const data = [
+    { key: 'Pizza Translator' },
+    { key: 'ScrollView Example' },
+    { key: 'FlatList Basics' },
+    { key: 'SectionList Basics' },
+    { key: 'Display An Image' }, 
+    { key: 'Cat' },  
+    { key: 'Cafe' },  
+    { key: 'CatApp' },  
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <VirtualizedList
+      data={data}
+      renderItem={({ item }) => {
+        switch (item.key) {
+          case 'Pizza Translator':
+            return <PizzaTranslator />;
+          case 'ScrollView Example':
+            return <ScrollViewExample />;
+          case 'FlatList Basics':
+            return <FlatListBasics />;
+          case 'SectionList Basics':
+            return <SectionListBasics />;
+          case 'Display An Image': 
+            return <DisplayAnImage />;
+          case 'Cat': 
+            return <Cat />;
+          case 'Cafe':  
+            return <Cafe />;
+          case 'CatApp': 
+            return <CatApp />;
+          default:
+            return null;
+        }
+      }}
+      keyExtractor={(item) => item.key}
+      getItemCount={() => data.length} 
+      getItem={(data, index) => data[index]} 
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
